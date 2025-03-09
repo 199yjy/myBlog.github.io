41b31d86d4f156f37be353b868624b4e7d425be8
yjy <2192242196@qq.com>
2025-03-09 11:34:40 +0800

    fix

--- docs/.vuepress/.temp/pages/get-started.html.js
+++ docs/.vuepress/.temp/pages/get-started.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/yanjieyan/workSpace/vuepressTest/vuepress-starter/docs/.vuepress/.temp/pages/get-started.html.vue"
-const data = JSON.parse("{\"path\":\"/get-started.html\",\"title\":\"Get Started\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Pages\",\"slug\":\"pages\",\"link\":\"#pages\",\"children\":[]},{\"level\":2,\"title\":\"Content\",\"slug\":\"content\",\"link\":\"#content\",\"children\":[]},{\"level\":2,\"title\":\"Configuration\",\"slug\":\"configuration\",\"link\":\"#configuration\",\"children\":[]},{\"level\":2,\"title\":\"Layouts and customization\",\"slug\":\"layouts-and-customization\",\"link\":\"#layouts-and-customization\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"get-started.md\"}")
+const data = JSON.parse("{\"path\":\"/get-started.html\",\"title\":\"Get Started\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Pages\",\"slug\":\"pages\",\"link\":\"#pages\",\"children\":[]},{\"level\":2,\"title\":\"Content\",\"slug\":\"content\",\"link\":\"#content\",\"children\":[]},{\"level\":2,\"title\":\"Configuration\",\"slug\":\"configuration\",\"link\":\"#configuration\",\"children\":[]},{\"level\":2,\"title\":\"Layouts and customization\",\"slug\":\"layouts-and-customization\",\"link\":\"#layouts-and-customization\",\"children\":[]}],\"git\":{\"updatedTime\":1741437057000,\"contributors\":[{\"name\":\"yjy\",\"username\":\"yjy\",\"email\":\"2192242196@qq.com\",\"commits\":1,\"url\":\"https://github.com/yjy\"}],\"changelog\":[{\"hash\":\"03ca8b2eb322980cb407c9db45b80b2ca55fad31\",\"date\":1741437057000,\"email\":\"2192242196@qq.com\",\"author\":\"yjy\",\"message\":\"init\"}]},\"filePathRelative\":\"get-started.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
